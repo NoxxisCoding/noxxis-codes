21dacf3f531d4d4dcea33491f5835d5c36ac0635
NoxxisCoding <jfarph@gmail.com>
2020-06-02 07:35:38 +0800

    Adding my Greetings Class

---
+++ Greetings.py
@@ -0,0 +1,13 @@
+class Greetings:
+    def __init__(self, theGreeting):
+        self.theGreeting = theGreeting
+    pass
+
+myGreeting = Greetings("Hello Git!")
+
+print(myGreeting.theGreeting)
+
+
+
+
+
